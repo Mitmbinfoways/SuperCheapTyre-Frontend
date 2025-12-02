115a9b3958fde7c86e8d9fe083253384016ed91c
MEETSIDHDHAPURA <meet.mbinfoways@gmail.com>
2025-12-02 11:06:12 +0530

    feat: Add initial frontend build with SEO metadata and lazy-loading for third-party scripts.

--- dist/assets/Toast-DkPb5xxS.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./react-vendor-DAyRi5Wp.js";const r="GLOBAL_SINGLE_TOAST",a=({message:t,type:o="default",autoClose:s=3e3})=>{e.isActive(r)||e(t,{toastId:r,type:o,position:"top-right",autoClose:s,hideProgressBar:!1,closeOnClick:!0,pauseOnHover:!0,draggable:!0,progress:void 0})};export{a as T};
--- dist/assets/router-8ngtUVIW.js
+++ dist/assets/router-8ngtUVIW.js
@@ -1,4 +1,4 @@
-import{r as i}from"./react-vendor-DAyRi5Wp.js";/**
+import{r as i}from"./react-vendor-Dj5e1rff.js";/**
  * react-router v7.9.2
  *
  * Copyright (c) Remix Software Inc.
